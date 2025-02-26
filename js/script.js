080de8a6bb299abd344e5683cedb292e30a710b7
sade-oc <112165291+sade-oc@users.noreply.github.com>
2025-02-26 12:03:08 +0000

    finished my basic level portfolio

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// Smooth Scrolling for Navigation Links
+document.querySelectorAll('nav ul li a').forEach(link => {
+   link.addEventListener('click', function (e) {
+     e.preventDefault();
+     const targetId = this.getAttribute('href').substring(1);
+     const targetSection = document.getElementById(targetId);
+     targetSection.scrollIntoView({ behavior: 'smooth', block: 'start' });
+   });
+ });
+ 
